326b12442618043c6f31a26526a5adf1d8a7a28f
Ghulam-Mustafa08291 <gm08291@st.habib.edu.pk>
2025-07-30 01:42:09 +0500

    registration page front end done

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,21 +1,56 @@
 import './LoginPage.css'; // Importing CSS file
+import {useState} from 'react';
 
 function LoginPage() {
+
+    const [inpPhoneNumber,setInpPhoneNumber]= useState('');
+    const [inpPassword,setInpPassword]=useState('');
+    const [error, setError] = useState({
+        phoneNumberError: "wrong phone number",
+        passwordError: "wrong password"
+    });
+
+
+    const onPhoneNumberChange = (e) => {
+       setInpPhoneNumber(e.target.value);
+        
+    }
+
+    const onPasswordChange = (e)=> {
+        setInpPassword(e.target.value)
+    }
+
+    const handleLogin = () =>{
+        if (inpPhoneNumber.length !=11){
+            setError({...error,phoneNumberError:"incorrect phone number"})
+            alert(error.phoneNumberError)
+        }
+
+        if (inpPassword.length <1){
+            setError({...error,passwordError:"Password field cant be empty!"})
+            alert(error.passwordError)
+        }
+    }
+
   return (
     <div className="login-wrapper">
         <div className="login-container">
         <h2>Login</h2>
         <input
+            value={inpPhoneNumber}
+            onChange={onPhoneNumberChange}
             type="text"
             placeholder="Enter your Phone Number"
             className="login-input"
         />
         <input
+            value={inpPassword}
+            onChange={onPasswordChange}
             type="password"
             placeholder="Enter your password"
             className="login-input"
         />
-        <button className="login-button">Login</button>
+        <button onClick={handleLogin} className="login-button">Login</button>
         </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import LoginPage from './LoginPage.jsx'
+import RegistrationPage from './RegistrationPage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage />
+    <RegistrationPage />
   </StrictMode>,
 )
